80006839a49b24919f25426cab3ef31e3a92226e
Jerry Wu <jwu1100@gmail.com>
2016-05-17 00:10:06 -0600

    Added array that logs the groupings

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $('#textbox').on('keyup', function() {
     var word = $('#textbox').val().toUpperCase(); // get word, case-insensitive
     var chunk_size = 0; // store "chunks" or groups of matching characters
     var stored_arr = []; // mismatched letters, stored to check for groupings
+    var group_arr = []; // store grouped letters for UI and visualization
 
     // Go through each letter (not actually going through everything)
     for (var i = 0; i < word.length; i++) {
@@ -13,6 +14,7 @@ $('#textbox').on('keyup', function() {
         if (head >= tail) {
             if (head === tail) {
                 chunk_size += 1; // middle letter is grouped with itself
+                group_arr.push(head);
             }
             break;
         }
@@ -23,6 +25,7 @@ $('#textbox').on('keyup', function() {
         if (stored_arr.length === 0) {
             if (head_char === tail_char) { // no mismatched letters means valid chunks
                 chunk_size += 2;
+                group_arr.push([head, head]);
             } else { // insert mismatched head to beginning, tail to end
                 stored_arr.unshift(head_char);
                 stored_arr.push(tail_char);
@@ -41,14 +44,17 @@ $('#textbox').on('keyup', function() {
                 }
                 if (is_group) {
                     chunk_size += 2;
+                    group_arr.push(stored_arr);
                     stored_arr = [];
                 }
             }
         }
     }
 
-    if (chunk_size === 0) { // minimum chunk sie is 1 (entire word is one group)
+    if (chunk_size === 0) { // no matches at all, entire word is one group
         chunk_size = 1;
+        group_arr.push(word);
     }
     console.log(chunk_size);
+    console.log(group_arr);
 });
