307f9fdfeb53aa982c7f6938c51ee9369ecbcdd6
Jerry Wu <jwu1100@gmail.com>
2016-05-17 01:47:36 -0600

    Fixed bugs with separating chunks

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Process text on keypress
 $('#textbox').on('keyup', function() {
     var word = $('#textbox').val().toUpperCase(); // get word, case-insensitive
-    var chunk_size = 0; // store "chunks" or groups of matching characters
-    var stored_arr = []; // mismatched letters, stored to check for groupings
-    var group_arr = []; // store grouped letters for UI and visualization
+    var chunk_size = 0; // number of chunks in string
+    var stored_arr = []; // keep track of incomplete chunks
+    var group_arr = []; // separate chunks for UI and visualization
 
-    // Go through each letter (not actually going through everything)
+    // Go through each letter (only up until midpoint)
     for (var i = 0; i < word.length; i++) {
         var head = i;
         var tail = word.length-1-i;
@@ -14,41 +14,41 @@ $('#textbox').on('keyup', function() {
 
         // Exit once the head and tail meet up
         if (head >= tail) {
-            if (head === tail) {
-                if (stored_arr.length === 0) { // no mismatched characters, middle char is own chunk
-                    group_arr.push(head_char);
-                    chunk_size += 1;
-                }
-            } else { // mismatched characters form a single chunk
+            if (head === tail) { // combine incomplete chunks to form a single chunk
                 stored_arr.splice(stored_arr.length/2, 0, head_char);
-                group_arr.push(stored_arr.join(''));
-                chunk_size  += 1;
+                group_arr.splice(group_arr.length/2, 0, stored_arr.join(''));
+            } else {
+                group_arr.splice(group_arr.length/2, 0, stored_arr.join(''));
             }
+            chunk_size  += 1;
             break;
         }
 
         // Compare letters
         if (stored_arr.length === 0) {
-            if (head_char === tail_char) { // no mismatched letters means valid chunks
+            if (head_char === tail_char) { // no chunks in progress so these are their own chunks
                 chunk_size += 2;
-                group_arr.push(head_char);
-            } else { // store unmatched head and tail
+                group_arr.splice(group_arr.length/2, 0, head_char, head_char);
+            } else { // initialize a chunk in progress
                 stored_arr.push(head_char, tail_char);
             }
         } else {
-            stored_arr.splice(stored_arr.length/2, 0, head_char, tail_char); // store in middle
-            if (head_char !== tail_char) { // groups possible only for mismatched chars
+            stored_arr.splice(stored_arr.length/2, 0, head_char, tail_char);
+            if (head_char !== tail_char) { // chunks are only possible on mismatched characters
                 var is_group = true;
                 var midpoint = stored_arr.length/2;
-                for (var j = 0; j < midpoint; j++) { // test for groups
+                for (var j = 0; j < midpoint; j++) { // test if chunk is valid
                     if (stored_arr[j] !== stored_arr[j+midpoint]) {
                         is_group = false;
                         break;
                     }
                 }
-                if (is_group) {
+                if (is_group) { // if chunk is complete, join with others
                     chunk_size += 2;
-                    group_arr.push(stored_arr.join('').substr(0, stored_arr.length/2));
+                    var stored_str = stored_arr.join('');
+                    var head_str = stored_str.substr(0, stored_str.length/2);
+                    var tail_str = stored_str.substr(stored_str.length/2, stored_str.length);
+                    group_arr.splice(group_arr.length/2, 0, head_str, tail_str);
                     stored_arr = [];
                 }
             }
@@ -56,5 +56,4 @@ $('#textbox').on('keyup', function() {
     }
     console.log(chunk_size);
     console.log(group_arr);
-    console.log('test');
 });
